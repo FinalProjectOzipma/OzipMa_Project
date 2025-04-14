c0aae6925815c5da0001d3d0e08cb10244973bc1
hannah896 <phn2260@naver.com>
2025-04-15 00:58:06 +0900

    🙈WIP: “생성시 비동기 함수 추가중”
    비동기로 MyUnit 자식에 root를 생성해줌

--- Assets/02.Scripts/Game/Entity/MyUnits/MyUnitController.cs
+++ Assets/02.Scripts/Game/Entity/MyUnits/MyUnitController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MyUnitController : EntityController
 {
+    public MyUnit MyUnit { get; private set; }
+    public MyUnitStatus MyUnitStatus { get; private set; }
+
     private void Awake()
     {
         Init();
@@ -14,5 +17,26 @@ public class MyUnitController : EntityController
         base.Init();
         AnimData = new MyUnitAnimationData();
         AnimData.Init(this);
+    }
+
+
+    //public override void Init(MyUnit myUnit)
+    //{
+    //    TakeRoot(myUnit);
+    //    base.Init();
+    //    AnimData = new MyUnitAnimationData();
+    //    AnimData.Init(this);
+    //}
+
+    //Root부분 생성해주는 파트
+    public void TakeRoot(MyUnit myUnit)
+    {
+        Managers.Resource.Instantiate(nameof(myUnit), go =>
+        {
+            MyUnit = myUnit;
+            MyUnitStatus = MyUnit.Status;
+            go.transform.SetParent(transform);
+            Debug.Log("자식 붙여드렸습니다");
+        });
     }
 }
