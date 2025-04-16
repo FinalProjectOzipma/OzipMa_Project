ce6332f431bc675e01048cc423f4160c2bbee912
yndoo <isabelle2k@naver.com>
2025-04-16 13:04:38 +0900

    ✨Feat: "TowerStatus 수정, RangeTowerController 추가"

--- Assets/02.Scripts/Game/Entity/Towers/ProjectileTowerController.cs
+++ Assets/02.Scripts/Game/Entity/Towers/ProjectileTowerController.cs
@@ -6,20 +6,21 @@ public class ProjectileTowerController : TowerControlBase
 {
     private EnemyController target;
     //private TowerProjectile Projectile;
-    private string projectileKey = "TowerProjectile";
+    //private string projectileKey = "TowerProjectile";
 
     protected override void Awake()
     {
-        Managers.Resource.LoadAssetAsync<GameObject>(projectileKey);
+        base.Awake();
+        //Managers.Resource.LoadAssetAsync<GameObject>(projectileKey);
     }
 
     public override void Attack(float AttackPower)
     {
-        Managers.Resource.Instantiate(projectileKey, go => {
-            // TODO 
-            // 1. Projectile을 enemy로 발사
-            // 2. 발사체의 공격력은 AttackPower로 설정해주기
-        });
+        //Managers.Resource.Instantiate(projectileKey, go => {
+        //    // TODO 
+        //    // 1. Projectile을 enemy로 발사 (Projectile에서 Type들을 적용 시켜야 함)
+        //    // 2. 발사체의 공격력은 AttackPower로 설정해주기
+        //});
     }
 
     // 감지된 적 1개만 계속 공격하도록
