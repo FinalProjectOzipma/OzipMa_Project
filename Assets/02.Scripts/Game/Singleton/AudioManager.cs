d728db1f2b52700057a019cadb2223a6fb2824d1
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-04-17 23:33:10 +0900

    👷 Chore: “Auido -> Audio 수정”

--- Assets/02.Scripts/Game/Singleton/AudioManager.cs
+++ Assets/02.Scripts/Game/Singleton/AudioManager.cs
@@ -24,33 +24,6 @@ public class AudioManager
             audioControler.Initialize();
             audioControler.audioManager = this;
         });
-
-        //Addressables.LoadResourceLocationsAsync("Audio").Completed += handle =>
-        //{
-        //    if (handle.Status != AsyncOperationStatus.Succeeded)
-        //    {
-        //        Debug.LogError("Audio 리소스 로딩 실패");
-        //        return;
-        //    }
-
-        //    foreach (var location in handle.Result)
-        //    {
-        //        var key = location.PrimaryKey;
-
-        //        if (key.Contains("AudioControler"))
-        //        {
-        //            Addressables.LoadAssetAsync<GameObject>(location).Completed += assetHandle =>
-        //            {
-        //                Debug.Log("오디오컨트롤러");
-        //                GameObject go = GameObject.Instantiate(assetHandle.Result);
-        //                audioControler = go.GetComponent<AudioControler>();
-        //                go.transform.SetParent(Managers.Instance.transform);
-        //                audioControler.Initialize();
-        //                audioControler.audioManager = this;
-        //            };
-        //        }
-        //    }
-        //};
     }
 
 
