5f1f7bc885283d64cd37c794bec25af29924594a
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-04-19 09:36:19 +0900

    📝 Docs: “주석 추가 및 주석 삭제”

--- Assets/02.Scripts/Game/UI/Scene/UI_Loading.cs
+++ Assets/02.Scripts/Game/UI/Scene/UI_Loading.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Threading;
 
 public class UI_Loading : UI_Base
 {
@@ -11,8 +12,7 @@ public class UI_Loading : UI_Base
     }
 
     public static string NextScene = "YGM_Research";
-    float minWaitTime = 1.0f;
-    float elapsedTime = 0f;
+
 
     private void Start()
     {
@@ -27,44 +27,26 @@ public class UI_Loading : UI_Base
         GetImage((int)Images.LoadBar).fillAmount = 0.0f;
     }
 
+    /// <summary>
+    /// 로딩씬 로드하고 씬전환
+    /// </summary>
     public static void LoadScene(string sceneName)
     {
         NextScene =  sceneName;
         UnityEngine.SceneManagement.SceneManager.LoadScene("Ygm_LoadingScene");
     }
 
-
+    /// <summary>
+    /// 씬전화 로딩
+    /// </summary>
     IEnumerator LoadSceneProcess()
     {
         AsyncOperation op = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(NextScene);
-        
+
         op.allowSceneActivation = false;
 
-        //while(!op.isDone)
-        //{
-        //    yield return null;
-        //    elapsedTime += Time.unscaledDeltaTime;
-
-        //    if (op.progress <  0.9f)
-        //    {
-        //        GetImage((int)Images.LoadBar).fillAmount = op.progress;
-        //    }
-        //    else
-        //    {
-
-        //        float fill = Mathf.Lerp(0.9f, 1f, (elapsedTime - minWaitTime));
-        //        GetImage((int)Images.LoadBar).fillAmount = fill;
-        //        //timer += Time.unscaledDeltaTime;
-        //        //GetImage((int)Images.LoadBar).fillAmount = Mathf.Lerp(0.9f, 1f, timer);
-
-        //        if (fill > 1.0f)
-        //        {
-        //            op.allowSceneActivation = true;
-        //            yield break;
-        //        }
-        //    }
-        //}
-
+        float minWaitTime = 1.0f;
+        float elapsedTime = 0f;
         while (!op.isDone)
         {
             yield return null;
