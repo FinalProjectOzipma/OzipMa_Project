27ace3f7603aefedb2f0b0f7d7757da0c598fe64
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-04-20 20:01:26 +0900

    ✨ Feat: “Core 구현”

--- Assets/02.Scripts/CoreBase.cs
+++ Assets/02.Scripts/CoreBase.cs
@@ -6,22 +6,31 @@ using UnityEngine.UI;
 
 public class CoreBase : MonoBehaviour
 {
-    public FloatBase Health;
-    public FloatBase MaxHelaht;
+    public FloatBase Health = new();
+    public FloatBase MaxHealth = new();
     public GameObject HpBar;
     private Image hpImage;
+    private float spawnY = 2.7f;
     // Start is called before the first frame update
 
     private void Awake()
     {
+        Health.Init(100.0f);
+        MaxHealth.Init(100.0f);
+
         hpImage = HpBar.GetComponent<Image>();
-        hpImage.fillAmount = Health.Value / MaxHelaht.Value;
+        hpImage.fillAmount = Health.Value / MaxHealth.Value;
+
+        float randomX = Random.Range(-2.0f,2.0f);
+
+        this.gameObject.transform.position = new Vector2(randomX, spawnY);
     }
 
     public void TakeDamge(float damage)
     {
+
         Health.Value = Mathf.Min(Health.Value-damage,0);
-        hpImage.fillAmount = Health.Value / MaxHelaht.Value;
+        hpImage.fillAmount = Health.Value / MaxHealth.Value;
 
         if(Health.Value == 0)
         {
@@ -32,8 +41,10 @@ public class CoreBase : MonoBehaviour
     public void UpgradeHealth(float value)
     {
         Health.AddValue(value);
-        MaxHelaht.AddValue(value);
-        hpImage.fillAmount = Health.Value / MaxHelaht.Value;
+        MaxHealth.AddValue(value);
+        hpImage.fillAmount = Health.Value / MaxHealth.Value;
     }
+
+
  
 }
