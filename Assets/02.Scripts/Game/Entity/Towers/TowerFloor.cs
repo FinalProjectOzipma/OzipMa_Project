068ee659e5edb482e0e2b9370d70fde306d4a281
yndoo <isabelle2k@naver.com>
2025-04-21 01:39:23 +0900

    ✨ Feat: "FloorTowerController에서 장판 생성 구현, TowerFloor 클래스 및 프리팹 추가"

---
+++ Assets/02.Scripts/Game/Entity/Towers/TowerFloor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerFloor : MonoBehaviour
+{
+    public void Init(string floorKey, Vector3 position, float attackPower, Tower ownerTower)
+    {
+        // TODO :: 장판 공격 시작 
+        // body 불러오기
+        // 장판딜 적용
+    }
+}
