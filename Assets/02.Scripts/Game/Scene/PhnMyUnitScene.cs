5c85a9d74ef73fbc573f8526640eee0af42f2234
hannah896 <phn2260@naver.com>
2025-04-22 14:34:18 +0900

    ✨ Feat: "스켈레톤 궁수 추가 및 렌더 우선순위 정렬"
    URP설정내의 소팅설정을 y1로 설정했어요

--- Assets/02.Scripts/Game/Scene/PhnMyUnitScene.cs
+++ Assets/02.Scripts/Game/Scene/PhnMyUnitScene.cs
@@ -11,6 +11,10 @@ public class PhnMyUnitScene : GameScene
     public override void Enter()
     {
         base.Enter();
+        MyUnit unit = new MyUnit();
+        unit.Init(1, null);
+        Managers.Player.Inventory.Add<MyUnit>(unit);
+        Managers.Wave.StartWave(0);
     }
 
     public override void Exit()
