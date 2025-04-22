bc5e77156bcae55146522f0804855c7d61740af8
yndoo <isabelle2k@naver.com>
2025-04-22 11:05:52 +0900

    ⚡️Add: "테스트용 씬 PydTowerScene추가"

--- Assets/02.Scripts/Game/Entity/Towers/TowerControlBase.cs
+++ Assets/02.Scripts/Game/Entity/Towers/TowerControlBase.cs
@@ -40,6 +40,10 @@ public abstract class TowerControlBase : MonoBehaviour
 
     private void Update()
     {
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            Managers.Scene.ChangeScene<PydTowerScene>(Managers.Scene.PydTowerScene);
+        }
         if (!IsPlaced) return;
         if(detectedEnemies.Count == 0) return;
 
