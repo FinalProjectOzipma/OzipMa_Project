ed18851980218a474516928b97ad89ff0395cab2
yndoo <isabelle2k@naver.com>
2025-04-25 15:46:55 +0900

    ✨ Feat: "ProjectileTower Flip 기능 추가"

--- Assets/02.Scripts/Game/Entity/Towers/TowerBodyBase.cs
+++ Assets/02.Scripts/Game/Entity/Towers/TowerBodyBase.cs
@@ -7,6 +7,11 @@ public class TowerBodyBase : MonoBehaviour
     public Animator Anim {  get; private set; }
     public TowerAnimationData AnimData { get; private set;}
 
+    public Vector3 FirePosition
+    {
+        get => gameObject.transform.GetChild(transform.childCount - 1).position;
+    }
+
     protected virtual void Awake()
     {
         Anim = Util.FindComponent<Animator>(gameObject, "MainSprite");
