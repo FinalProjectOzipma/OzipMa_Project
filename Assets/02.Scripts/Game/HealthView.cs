c1e17c3587205a763c7f806c4c338c5e545cf088
hannah896 <phn2260@naver.com>
2025-04-29 15:15:13 +0900

    🐛 Fix: "유닛의 체력바 적용"

--- Assets/02.Scripts/Game/HealthView.cs
+++ Assets/02.Scripts/Game/HealthView.cs
@@ -16,9 +16,23 @@ public class HealthView : MonoBehaviour
         ctrl = GetComponentInParent<EntityController>();
     }
 
-    public void SetHpBar(float amount, float maxAmount)
+    private void Update()
     {
         FillRect.fillOrigin = -ctrl.FacDir;
+    }
+
+    /// <summary>
+    /// 체력바 업데이트
+    /// </summary>
+    /// <param name="amount"></param>
+    /// <param name="maxAmount"></param>
+    public void SetHpBar(float amount, float maxAmount)
+    {
         FillRect.fillAmount = amount / maxAmount;
+    }
+
+    private void OnDisable()
+    {
+        FillRect.fillAmount = 1f;
     }
 }
