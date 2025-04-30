788314a0361fc26c20a5e8c50acc5dcbe59dd340
ByeonSang <85797062+ByeonSang@users.noreply.github.com>
2025-04-30 17:54:48 +0900

    🐛Fix: "EffectSpriteRenderer 수정"
    Effect남아있는 현상 수정

---
+++ Assets/02.Scripts/Game/Entity/EffectRenderer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectRenderer : MonoBehaviour
+{
+    private SpriteRenderer[] effects;
+
+    private void Awake()
+    {
+        effects = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    private void OnDisable()
+    {
+        foreach (var effect in effects)
+            effect.sprite = null;  
+    }
+}
