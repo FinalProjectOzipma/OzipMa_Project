c48177c9b3b162c68f4dcf4e7bfe8a97a261b78c
hannah896 <phn2260@naver.com>
2025-05-10 16:57:49 +0900

    🙈 WIP: "가챠 추가중"

--- Assets/02.Scripts/Game/Gacha/GachaSystem.cs
+++ Assets/02.Scripts/Game/Gacha/GachaSystem.cs
@@ -6,21 +6,43 @@ using UnityEngine;
 public class GachaSystem : MonoBehaviour
 {
     RankType rankType;
-    public MyUnit GetRandomUnit(List<MyUnit> allUnits)
+    public MyUnit GetRandomUnit()
     {
         int rand = Random.Range(0, 100);
         RankType selectedRank;
 
-        if (rand < 20) selectedRank = RankType.Normal; //20%
-        else if (rand < 40) selectedRank = RankType.Rare; //20%
-        else if (rand < 60) selectedRank = RankType.Epic; //20%
-        else if (rand < 80) selectedRank = RankType.Myth; //20%
-        else if (rand < 95) selectedRank = RankType.Legend; //15%
-        else selectedRank = RankType.Ancient; // 5%
+        if (rand < 70) selectedRank = RankType.Normal; //70%
+        else selectedRank = RankType.Rare; //임시용
+        //else if (rand < 90) selectedRank = RankType.Rare; //20%
+        //else if (rand < 98) selectedRank = RankType.Epic; //8%
+        //else selectedRank = RankType.Legend; //2%
 
-        var candidates = allUnits.Where(unit => unit.RankType == selectedRank).ToList();
-        if (candidates.Count == 0) return null;
+        var result = Util.TableConverter<DefaultTable.Gacha>(Managers.Data.Datas[Enums.Sheet.Gacha]);
+        var row = result[(int)selectedRank];
+        int key = row.Key[Random.Range(0, row.Key.Count)];
+        MyUnit unit = new();
+        //스프라이트 어떻게 넣어주지..?
+        //unit.Init(key, );
+        return unit;
+    }
+    public Tower GetRandomTower()
+    {
+        int rand = Random.Range(0, 100);
+        RankType selectedRank;
 
-        return candidates[Random.Range(0, candidates.Count)];
+        if (rand < 70) selectedRank = RankType.Normal; //70%
+        else selectedRank = RankType.Rare; //임시용
+        //else if (rand < 90) selectedRank = RankType.Rare; //20%
+        //else if (rand < 98) selectedRank = RankType.Epic; //8%
+        //else selectedRank = RankType.Legend; //2%
+
+        var result = Util.TableConverter<DefaultTable.Gacha>(Managers.Data.Datas[Enums.Sheet.Gacha]);
+        var row = result[(int)selectedRank];
+        int key = row.Key[Random.Range(0, row.Key.Count)];
+        Tower tower = new();
+        //스프라이트 어떻게 넣어주지..?
+        //Sprite image = Managers.Resource.;
+        //tower.Init(key, image);
+        return tower;
     }
 }
