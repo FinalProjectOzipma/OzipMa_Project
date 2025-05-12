d7997358714f8b02dde3ff0e0eb5a6b700530be7
yndoo <isabelle2k@naver.com>
2025-05-12 11:36:53 +0900

    ✨ Feat: "Tower 공격사거리 표시기 크기 업데이트 가능"

--- Assets/02.Scripts/Game/Entity/Towers/TowerControlBase.cs
+++ Assets/02.Scripts/Game/Entity/Towers/TowerControlBase.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,6 +20,7 @@ public abstract class TowerControlBase : MonoBehaviour
 
     protected LinkedList<EnemyController> detectedEnemies = new(); // 범위 내 적들
     protected GameObject body; // 현재 나의 외형
+    protected TowerBodyBase towerBodyBase;
 
     private float attackCooldown = 0f;
     private CircleCollider2D range;
@@ -36,8 +38,7 @@ public abstract class TowerControlBase : MonoBehaviour
 
     public void Init()
     {
-        range = GetComponent<CircleCollider2D>();
-        range.radius = TowerStatus == null ? 1f : TowerStatus.AttackRange.GetValue();
+        ApplyAttackRange(TowerStatus.AttackRange.GetValue());
     }
 
     private void Update()
@@ -73,6 +74,7 @@ public abstract class TowerControlBase : MonoBehaviour
     /// </summary>
     public void TowerStart()
     {
+        Init();
         IsPlaced = true;
         StartAnimation(AnimData.StartHash);
     }
@@ -109,6 +111,7 @@ public abstract class TowerControlBase : MonoBehaviour
 
     public TowerBodyBase GetTowerBodyBase()
     {
+        if(towerBodyBase != null) return towerBodyBase;
         if(body != null)
         {
             return body.GetComponent<TowerBodyBase>();
@@ -150,6 +153,13 @@ public abstract class TowerControlBase : MonoBehaviour
     {
         if(range == null) range = GetComponent<CircleCollider2D>();
         range.radius = TowerStatus == null ? 1f : newValue;
-        Util.Log($"어택레인지 {newValue}만큼으로 늘어났다");
+
+        // 타워 사거리 표시기 업데이트
+        if(body != null)
+        {
+            GetTowerBodyBase().AttackRangeObj.transform.localScale = Vector3.one * newValue;
+        }
+
+        Util.Log($"{Name} 타워의 어택레인지 {newValue}만큼으로 세팅됐다");
     }
 }
