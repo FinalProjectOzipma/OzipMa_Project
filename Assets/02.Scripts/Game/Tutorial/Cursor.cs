b8db88619d10d3747da816a8bb1c4ff071a072b8
hannah896 <phn2260@naver.com>
2025-05-20 20:26:08 +0900

    ✨ Feat: "커서, 다이얼로그 세팅완료 "
    유니티 씬 내에도 넣어놓다

--- Assets/02.Scripts/Game/Tutorial/Cursor.cs
+++ Assets/02.Scripts/Game/Tutorial/Cursor.cs
@@ -4,27 +4,46 @@ using UnityEngine;
 
 public class Cursor : MonoBehaviour
 {
+    public enum Type 
+    {
+        Click,
+        Drag,
+    }
+
     private Vector3 startPos;
     private Vector3 endPos;
 
-    private Tweener movingTweener;
+    private Tweener tweener;
+
     public void Init(Vector3 startPos, Vector3 endPos)
     {
         this.startPos = startPos;
         this.endPos = endPos;
         gameObject.transform.position = startPos;
 
-        //무한 드래그
-        movingTweener = transform
-            .DOMove(endPos, 5f)
-            .SetEase(Ease.InOutQuart)
-            .SetLoops(-1, LoopType.Restart)
-            .SetAutoKill(false);
+        //무한 드래그 애니메이션
+        if (startPos != endPos)
+        {
+            tweener = transform
+                .DOMove(endPos, 5f)
+                .SetEase(Ease.InOutQuart)
+                .SetLoops(-1, LoopType.Restart)
+                .SetAutoKill(false);
+        }
+        //무한 클릭 애니메이션
+        else if (startPos == endPos)
+        {
+            tweener = transform
+                .DORotate(new Vector3(0f, 0f, 45f), 5f)
+                .SetEase(Ease.InOutQuart)
+                .SetLoops(-1, LoopType.Restart)
+                .SetAutoKill(false);
+        }
     }
 
     private void OnDisable()
     {
-        movingTweener.Kill();
+        tweener.Kill();
         Managers.Resource.Destroy(gameObject);
     }
 }
--- Assets/02.Scripts/Game/Tutorial/Dialogue.cs
+++ Assets/02.Scripts/Game/Tutorial/Dialogue.cs
@@ -5,35 +5,71 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Dialogue : MonoBehaviour 
+public class Dialogue : UI_Base
 {
-    public Image BG;
-
+    // 타이핑 간격시간
+    public float timegap;
+    //대화 종료 bool값
     public bool IsEnd;
-    [SerializeField] private TextMeshProUGUI TutorialTxt;
-    [SerializeField] private Image Cursor;
-    [SerializeField] private Button Bg;
 
+    //타이핑 애니메이션 코루틴
+    private Coroutine Play;
+    private WaitForSeconds time;
+
+    //튜토리얼 대사
+    [SerializeField] private TextMeshProUGUI TutorialTxt;
+    //대사 넘기는 버튼
+    [SerializeField] private Button Box;
+
+    //대사 발사대
     private Queue<string> txt;
 
+    /// <summary>
+    /// 출력할 대사를 넣는 메서드(큐임)
+    /// </summary>
+    /// <param name="s"></param>
     public void EnQueueText(string s)
     {
         txt.Enqueue(s);
     }
 
+    private void Awake()
+    {
+        //시간 세팅
+        time = new WaitForSeconds(timegap);
+        //버튼메서드 등록
+        Box.onClick.AddListener(TxtOnClick);
+    }
+
     private void Start()
     {
+        //초기화
         Init();
     }
 
+    /// <summary>
+    /// 대화 연출 나올때 기본 세팅
+    /// </summary>
     public void Init()
     {
         Time.timeScale = 0f;
         TutorialTxt.text = txt.Dequeue();
     }
 
+    /// <summary>
+    /// 클릭 했을때 대화 메서드
+    /// </summary>
     public void TxtOnClick()
     {
+        //누르면 텍스트연출 스킵시키기
+        if (Play != null)
+        {
+            StopCoroutine(Play);
+            TutorialTxt.maxVisibleCharacters = TutorialTxt.text.Length;
+            Play = null;
+            return;
+        }
+
         if (txt.Count == 0)
         {
             IsEnd = true;
@@ -42,5 +78,18 @@ public class Dialogue : MonoBehaviour
         }
 
         TutorialTxt.text = txt.Dequeue();
+        TutorialTxt.maxVisibleCharacters = 0;
+        Play = StartCoroutine(Typing());
+    }
+
+    //타이핑 애니메이션 연출
+    private IEnumerator Typing()
+    {
+        int len = TutorialTxt.text.Length;
+        for (int i = 0; i<len; i++)
+        {
+            TutorialTxt.maxVisibleCharacters = i + 1;
+            yield return time;
+        }
     }
 }
