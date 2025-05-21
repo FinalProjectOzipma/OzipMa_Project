a85a7b111aab91d267c48c06c9fa2503af4e2cf1
yndoo <isabelle2k@naver.com>
2025-05-21 16:20:39 +0900

    ✨ Feat: "튜토리얼 - 연구 튜토리얼 완료"
    고봉밥 튜토리얼

---
+++ Assets/02.Scripts/Game/Tutorial/GameTutorial/ResearchTutorial.cs
@@ -0,0 +1,105 @@
+using DefaultTable;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResearchTutorial : TutorialBase
+{
+    private UI_Main mainUI;
+    private UI_ResearchScene researchUI;
+    private int dialogueNum = 0;
+
+    public ResearchTutorial(TutorialController _controller) : base(_controller)
+    {
+    }
+    public override bool CheckCondition()
+    {
+        Util.Log($"연구 튜토리얼 번호 {dialogueNum}");
+
+        switch (dialogueNum)
+        {
+            case 0:
+                // 연구탭 열렸는지 확인
+                if(mainUI.isResearchOpen == true)
+                {
+                    dialogueNum++;
+                    controller.ShowOnlyDialogue();
+
+                    // 연구 시작 버튼 커서 세팅
+                    Vector3 startPos = new Vector3(345, 315, 0);
+                    Vector3 endPos = new Vector3(345, 315, 0);
+                    controller.Cursor.Init(startPos, endPos, CursorType.Click);
+                    researchUI = Managers.UI.GetPopup<UI_ResearchScene>();
+                }
+                break;
+            case 1:
+                // 연구버튼 눌렀는지 확인
+                if(researchUI.AttackUpgrade.isResearching)
+                {
+                    dialogueNum++;
+                    controller.ShowOnlyDialogue();
+
+                    // 시간 단축 커서 세팅
+                    Vector3 startPos = new Vector3(-75, 242, 0);
+                    Vector3 endPos = new Vector3(-75, 242, 0);
+                    controller.Cursor.Init(startPos, endPos, CursorType.Click);
+                    Managers.Player.AddGold(1000);
+                }
+                break;
+            case 2:
+                // 시간 단축 눌렀는지 확인
+                if(researchUI.AttackUpgrade.isResearchGoldBtn)
+                {
+                    dialogueNum++;
+                    controller.ShowOnlyDialogue();
+
+                    // 완료 버튼 커서 세팅
+                    Vector3 startPos = new Vector3(345, 315, 0);
+                    Vector3 endPos = new Vector3(345, 315, 0);
+                    controller.Cursor.Init(startPos, endPos, CursorType.Click);
+                }
+                break;
+            case 3:
+                // 완료 버튼 눌렀는지 확인
+                if (researchUI.AttackUpgrade.isResearchComplete)
+                {
+                    dialogueNum++;
+                }
+                break;
+            default:
+                // 모두 끝났으면 튜토리얼 끝
+                return true;
+        }
+        return false;
+    }
+
+    public override void OnStart()
+    {
+        mainUI = Managers.UI.GetScene<UI_Main>();
+
+        // 연구 탭 클릭 커서 세팅
+        Vector3 startPos = new Vector3(140, -885, 0); 
+        Vector3 endPos = new Vector3(140, -885, 0);
+        controller.Cursor.Init(startPos, endPos, CursorType.Click);
+
+        controller.Dialogue.EnQueueText("이번에 소개해드릴 기능은 연구입니다.\n먼저 연구 탭을 클릭해보시겠어요?");
+        controller.Dialogue.EnQueueText("");
+        controller.Dialogue.EnQueueText("좋아요!!\n연구를 통해 우리가 더 강해질 수 있어요.");
+        controller.Dialogue.EnQueueText("공격력 연구 버튼을 클릭해보세요.");
+        controller.Dialogue.EnQueueText("");
+        controller.Dialogue.EnQueueText("좋아요!!\n그런데 연구가 시간이 좀 걸리네요?");
+        controller.Dialogue.EnQueueText("이럴 땐 재화를 사용하여 연구시간을 단축시킬 수 있어요!");
+        controller.Dialogue.EnQueueText("제 비상금..을 조금 드렸으니 골드로 시간을 단축시켜보시겠어요?");
+        controller.Dialogue.EnQueueText("");
+        controller.Dialogue.EnQueueText("마지막으로 완료 버튼을 클릭하면 연구가 완료됩니다!");
+        controller.Dialogue.EnQueueText("");
+
+        controller.SetDialogueActive(true);
+    }
+    public override void OnEnd()
+    {
+        base.OnEnd();
+
+        // TODO :: 튜토리얼 보상 처리
+    }
+}
